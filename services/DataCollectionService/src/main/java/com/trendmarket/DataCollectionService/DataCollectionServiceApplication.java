16ba9637a4e09d8a85ba8a058fd37f300815cc00
Minki23 <michal.tomasz.szymczak@gmail.com>
2026-03-29 21:03:56 +0200

    first commit

---
+++ services/DataCollectionService/src/main/java/com/trendmarket/DataCollectionService/DataCollectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trendmarket.DataCollectionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCollectionServiceApplication.class, args);
+	}
+
+}
